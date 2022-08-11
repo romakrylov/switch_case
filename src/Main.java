fb05bb997f3f5ce58ae968bb6a40d53e052f2663
Roman Krylov <0087897@gmail.com>
2022-08-11 15:45:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("");
+            String input = new Scanner(System.in).nextLine();
+
+//            if (input.equals("Привет!")) {
+//                System.out.println("Привет!");
+//            } else if (input.equals("Как дела?")) {
+//                System.out.println("Отлично! У тебя как?");
+//            } else {
+//                System.out.println("Не понимаю сообщение :(");
+//            }
+
+//            switch (input) {
+//                case "Привет!":
+//                    System.out.println("Привет!");
+//                    break;
+//                case "Как дела?":
+//                    System.out.println("Отлично! У тебя как?");
+//                    break;
+//                default:
+//                    System.out.println("Не понимаю сообщение :(");
+//            }
+
+            switch (input) {
+                case "Привет!" -> System.out.println("Привет!");
+                case "Как дела?" -> {
+                    System.out.println("Отлично!");
+                    System.out.println("У тебя как?");
+                }
+                default -> System.out.println("Не понимаю сообщение :(");
+            }
+        }
+    }
+}
